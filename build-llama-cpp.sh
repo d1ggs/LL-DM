5bcefb84106788ac88719c005ab40be8c838bdb6
d1ggs <elimdor@live.it>
2024-02-13 18:07:43 +0100

    Update build script

--- build-llama-cpp.sh
+++ build-llama-cpp.sh
@@ -1,6 +1,9 @@
-git clone -b v0.2.28 https://github.com/abetlen/llama-cpp-python
+rm -rf build || true
+mkdir build || true
+cd build
+git clone -b v0.2.41 https://github.com/abetlen/llama-cpp-python
 cd llama-cpp-python
 git submodule update --init --recursive
 # Build the wheel
-pdm use 3.10
-CMAKE_ARGS="-DLLAMA_CLBLAST=on" pdm build -v
+pdm use 3.10 -i
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" pdm build -v
